19f8d0a5723c31b94f9e344d69d137a3d8b05604
Caio Toledo <caioviniciusdetoledo@gmail.com>
2019-04-14 00:44:11 +0100

    Create map for LED configuration in RGB Library
    Signed-off-by: Caio Toledo <caioviniciusdetoledo@gmail.com>

--- thirdparty/rgb/src/LED_RGB.c
+++ thirdparty/rgb/src/LED_RGB.c
@@ -24,8 +24,62 @@
 #define MAX_VALUE		(WIDTH_BASE-1)
 #define MIN_VALUE		(0)
 
+typedef struct
+{
+	uint32_t GPIOPinConf;
+	uint32_t GPIOPinType;
+	uint32_t PWMGen;
+	uint32_t PWMOut;
+	uint32_t PWMOutBit;
+} tPWM_Pin_Conf;
+
+typedef struct
+{
+	LED_RGB led;
+	tPWM_Pin_Conf conf;
+} tLED_Conf;
+
+static const tPWM_Pin_Conf *GetPWMConf(LED_RGB led);
 static void LED_PWM_Configure(LED_RGB led);
 static void setLedDuty (LED_RGB led, uint32_t duty);
+
+/* Map for LEDs configurations: */
+static const tLED_Conf led_configurations[] =
+{
+	{
+		.led = RED_LED,
+		.conf =
+		{
+			.GPIOPinConf = GPIO_PF1_M1PWM5,
+			.GPIOPinType = GPIO_PIN_1,
+			.PWMGen = PWM_GEN_2,
+			.PWMOut = PWM_OUT_5,
+			.PWMOutBit = PWM_OUT_5_BIT,
+		},
+	},
+	{
+		.led = BLUE_LED,
+		.conf =
+		{
+			.GPIOPinConf = GPIO_PF2_M1PWM6,
+			.GPIOPinType = GPIO_PIN_2,
+			.PWMGen = PWM_GEN_3,
+			.PWMOut = PWM_OUT_6,
+			.PWMOutBit = PWM_OUT_6_BIT,
+		},
+	},
+	{
+		.led = GREEN_LED,
+		.conf =
+		{
+			.GPIOPinConf = GPIO_PF3_M1PWM7,
+			.GPIOPinType = GPIO_PIN_3,
+			.PWMGen = PWM_GEN_3,
+			.PWMOut = PWM_OUT_7,
+			.PWMOutBit = PWM_OUT_7_BIT,
+		},
+	}
+};
 
 void LED_Enable()
 {
@@ -49,39 +103,30 @@ void RGB_Set(uint32_t rValue, uint32_t gValue, uint32_t bValue)
 	setLedDuty(BLUE_LED, bValue);
 }
 
+static const tPWM_Pin_Conf *GetPWMConf(LED_RGB led)
+{
+	const tPWM_Pin_Conf *pwm_conf = 0U;
+	/* Search for PWM configuration */
+	for (uint8_t i = 0; i < sizeof(led_configurations)/sizeof(tLED_Conf); i++)
+	{
+		if (led_configurations[i].led == led)
+		{
+			pwm_conf = &led_configurations[i].conf;
+			break;
+		}
+	}
+	return pwm_conf;
+}
+
 static void LED_PWM_Configure(LED_RGB led)
 {
-	uint32_t GPIOPinConf;
-	uint32_t GPIOPinType;
-	uint32_t PWMGen;
-	uint32_t PWMOut;
-	uint32_t PWMOutBit;
+	const tPWM_Pin_Conf *pwm_conf;
 
-	switch (led) {
-		case RED_LED:
-			GPIOPinConf = GPIO_PF1_M1PWM5;
-			GPIOPinType = GPIO_PIN_1;
-			PWMGen = PWM_GEN_2;
-			PWMOut = PWM_OUT_5;
-			PWMOutBit = PWM_OUT_5_BIT;
-			break;
-		case BLUE_LED:
-			GPIOPinConf = GPIO_PF2_M1PWM6;
-			GPIOPinType = GPIO_PIN_2;
-			PWMGen = PWM_GEN_3;
-			PWMOut = PWM_OUT_6;
-			PWMOutBit = PWM_OUT_6_BIT;
-			break;
-		case GREEN_LED:
-			GPIOPinConf = GPIO_PF3_M1PWM7;
-			GPIOPinType = GPIO_PIN_3;
-			PWMGen = PWM_GEN_3;
-			PWMOut = PWM_OUT_7;
-			PWMOutBit = PWM_OUT_7_BIT;
-			break;
-		default:
-		//If the Value is wrong, stop the configuration:
-			return;
+	pwm_conf = GetPWMConf(led);
+	/* Return if no valid configuration was found */
+	if (pwm_conf == 0U)
+	{
+		return;
 	}
 
 	/* Configure Peripheral */
@@ -90,51 +135,40 @@ static void LED_PWM_Configure(LED_RGB led)
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
 
 	/* Configure Pins */
-	GPIOPinConfigure(GPIOPinConf);
-	GPIOPinTypePWM(GPIO_PORTF_BASE, GPIOPinType);
+	GPIOPinConfigure(pwm_conf->GPIOPinConf);
+	GPIOPinTypePWM(GPIO_PORTF_BASE, pwm_conf->GPIOPinType);
 
 	/* Configure PWM */
-	PWMGenConfigure(PWM1_BASE, PWMGen, PWM_GEN_MODE_UP_DOWN|PWM_GEN_MODE_NO_SYNC);
-	PWMGenPeriodSet(PWM1_BASE, PWMGen, PWM_PERIOD);
-	PWMOutputState(PWM1_BASE, PWMOutBit, true);
-	PWMOutputInvert(PWM1_BASE, PWMOutBit, false);
+	PWMGenConfigure(PWM1_BASE, pwm_conf->PWMGen, PWM_GEN_MODE_UP_DOWN|PWM_GEN_MODE_NO_SYNC);
+	PWMGenPeriodSet(PWM1_BASE, pwm_conf->PWMGen, PWM_PERIOD);
+	PWMOutputState(PWM1_BASE, pwm_conf->PWMOutBit, true);
+	PWMOutputInvert(PWM1_BASE, pwm_conf->PWMOutBit, false);
 
 	/* Start Duty Cycle */
-	PWMPulseWidthSet(PWM1_BASE, PWMOut, 0U);
+	PWMPulseWidthSet(PWM1_BASE, pwm_conf->PWMOut, 0U);
 
 	/* Enable PWM */
-	PWMGenEnable(PWM1_BASE, PWMGen);
+	PWMGenEnable(PWM1_BASE, pwm_conf->PWMGen);
 }
 
 static void setLedDuty (LED_RGB led, uint32_t duty)
 {
 	uint32_t base;
-	uint32_t PWMOut;
-	uint32_t PWMGen;
+	const tPWM_Pin_Conf *pwm_conf;
 
-	switch (led) {
-		case RED_LED:
-			PWMOut = PWM_OUT_5;
-			PWMGen = PWM_GEN_2;
-			break;
-		case BLUE_LED:
-			PWMOut = PWM_OUT_6;
-			PWMGen = PWM_GEN_3;
-			break;
-		case GREEN_LED:
-			PWMOut = PWM_OUT_7;
-			PWMGen = PWM_GEN_3;
-			break;
-		default:
-			return;
+	pwm_conf = GetPWMConf(led);
+	/* Return if no valid configuration was found */
+	if (pwm_conf == 0U)
+	{
+		return;
 	}
 
 	/* Limit Duty Cycle to MAX_VALUE */
 	duty = duty > MAX_VALUE ? MAX_VALUE : duty;
 
 	/* Calculate base duty cycle */
-	base = PWMGenPeriodGet(PWM1_BASE, PWMGen)*duty/WIDTH_BASE;
+	base = PWMGenPeriodGet(PWM1_BASE, pwm_conf->PWMGen)*duty/WIDTH_BASE;
 
 	/* Update Duty Cycle */
-	PWMPulseWidthSet(PWM1_BASE, PWMOut, base);
+	PWMPulseWidthSet(PWM1_BASE, pwm_conf->PWMOut, base);
 }
