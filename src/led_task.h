35b4708d94b1add361a6f2d7e0c568b00dc9e8e3
Caio Toledo <caioviniciusdetoledo@gmail.com>
2019-05-01 21:13:59 +0100

    Add doxygen doc for freertos tasks
    Signed-off-by: Caio Toledo <caioviniciusdetoledo@gmail.com>

--- src/led_task.h
+++ src/led_task.h
@@ -1,6 +1,11 @@
 #ifndef LED_TASK_H_
 #define LED_TASK_H_
 
+/**
+ * @brief Generate rainbow light in RGB LED
+ *
+ * @param pvParameters Unused parameter
+ */
 void vLedTask(void *pvParameters);
 
 #endif /* LED_TASK_H_ */
--- src/mpu6050_task.h
+++ src/mpu6050_task.h
@@ -1,6 +1,11 @@
 #ifndef MPU6050_TASK_H_
 #define MPU6050_TASK_H_
 
+/**
+ * @brief Task to interact with IMU MPU6050
+ *
+ * @param pvParameters Unused parameter
+ */
 void vMPU6050Task(void *pvParameters);
 
 #endif /* MPU6050_TASK_H_ */
